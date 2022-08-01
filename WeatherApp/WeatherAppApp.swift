1106347f1af7f9394370979dc89b1744acb40e9a
Ramez Nessim <rameznessim99@gmail.com>
2022-07-31 20:28:39 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ramez Nessim on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
